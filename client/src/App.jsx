51605eadba6f51e8a85f1e4ab0aae6063e769672
sujitIwale <56259235+sujitIwale@users.noreply.github.com>
2021-08-04 01:52:33 +0530

    fix(eslint): rules modified

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,30 +2,30 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit
-          <code>
-            src/App.js
-          </code>
-          and save to reload.
-        </p>
-        <p>
-          Just Checking for any commits from my account
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='App' style={{ backgroundColor: 'blue' }}>
+			<header className='App-header' style={{ color: 'red' }}>
+				<p>
+					Edit
+					<code>src/App.js</code>
+					and save to reload.
+				</p>
+				<p>
+					Edit
+					<code>src/App.js</code>
+					and save to reload.
+				</p>
+				<p>Just Checking for red commits from my account</p>
+				<a
+					className='App-link'
+					href='https://reactjs.org'
+					target='_blank'
+					rel='noopener noreferrer'>
+					Learn React
+				</a>
+			</header>
+		</div>
+	);
 }
 
 export default App;
